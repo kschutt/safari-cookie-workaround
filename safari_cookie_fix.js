75d62694c386219c63d84be7e4035d4036d7f4be
Kyle Schutt <kyle@revv.co>
2018-04-20 12:25:38 -0400

    updating safari cookie fix

--- safari_cookie_fix.js
+++ safari_cookie_fix.js
@@ -7,7 +7,7 @@ jQuery(function(){
     if (is_safari) {
       if (!document.cookie.match(/^(.*;)?\s*fixed\s*=\s*[^;]+(.*)?$/)) {
         document.cookie = 'fixed=fixed; expires=Tue, 19 Jan 2038 03:14:07 UTC; path=/';
-        window.location.replace("http://home.kammllc.com:8000/safari_fix.html");
+        window.location.replace("http://home.kammllc.com:3000/.well-known/safari-cookie-fix?page_uid=rv_page_mMorwSy3wzYHzrQcY8BDgZhW");
       };
     };
   })();
